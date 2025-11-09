9785c3ea932a0c99ecf6d35e99033bd88e8593fa
Mick Suraj <127860227+Micksuraj777@users.noreply.github.com>
2025-11-09 22:35:54 +0530

    feat: adjusted the filler

--- components/filler.tsx
+++ components/filler.tsx
@@ -4,13 +4,13 @@ export default function Filler() {
   return (
     <div className="relative mx-auto h-full max-w-screen-xl bg-[url('/image/image2.png')] bg-cover bg-center">
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="animate-glow rounded-full">
+        <div className="animate-slowBounce rounded-full">
           <Image
             src="/logos/techletics-logo.png"
             alt="Techletics Logo"
             height={300}
             width={300}
-            className="animate-slowBounce object-contain"
+            className="animate-glow rounded-full object-contain py-3 pl-7"
           />
         </div>
       </div>
