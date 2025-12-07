848963344e7ce74226300cf2c0683769ac408097
alfrinP <alfrinpoulose@gmail.com>
2025-12-07 17:06:54 +0530

    chore: update next.js to version 15.4.8 in package.json and package-lock.json

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,8 @@ import type { Metadata } from 'next';
 import Filler from '@/components/filler';
 import Memories from '@/components/memories';
 import Media from '@/components/media';
-import SectionSeparatorBottom from '@/components/custom/sectionSeparatorBottom';
-import SectionSeparatorTop from '@/components/custom/sectionSeparatorTop';
 import PrevEvents from '@/components/prevEvents';
+import SectionSeparator from '@/components/custom/sectionSeparator';
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -22,63 +21,57 @@ export default function HomePage() {
       <section className="relative bg-[url('/image/bg-white.png')] bg-cover bg-center">
         <Home />
 
-        <SectionSeparatorBottom />
+        <SectionSeparator position="top" />
       </section>
 
       <TextMarquee type="ignite" />
 
       <section className="relative bg-[url('/image/bg-white.png')] bg-cover bg-center">
-        <SectionSeparatorTop />
+        <SectionSeparator position="top" />
 
         <HomeAbout />
 
-        <SectionSeparatorBottom />
+        <SectionSeparator position="bottom" />
       </section>
 
       <TextMarquee type="techletics" />
 
       <section className="relative bg-[url('/image/ruins-bg-grey.png')] bg-cover bg-center py-[4rem] max-xl-wide:pb-[11rem] max-md:pb-[35rem]">
-        <SectionSeparatorTop />
+        <SectionSeparator position="top" />
 
         <span id="stats"></span>
 
         <Stats />
 
-        <SectionSeparatorBottom />
+        <SectionSeparator position="bottom" />
       </section>
 
-      {/* <TextMarquee type="techletics" /> */}
-
       <section className="relative flex h-[100vh] flex-col items-center justify-center bg-[url('/image/bg-white.png')] bg-cover bg-center">
-        <SectionSeparatorTop />
+        <SectionSeparator position="top" />
 
         <Memories />
 
-        <SectionSeparatorBottom />
+        <SectionSeparator position="bottom" />
       </section>
 
       <section className="relative bg-quarternary bg-[url('/image/footer.png')] bg-cover bg-center py-[5rem]">
-        <SectionSeparatorTop />
+        <SectionSeparator position="top" />
 
         <PrevEvents />
 
-        <SectionSeparatorBottom />
+        <SectionSeparator position="bottom" />
       </section>
-
-      {/* <TextMarquee type="ignite" /> */}
 
       <section className="relative h-[90vh] bg-black">
         <Filler />
       </section>
 
-      {/* <TextMarquee type="techletics" /> */}
-
       <section className="relative bg-[url('/image/bg-white.png')] bg-cover bg-center">
-        <SectionSeparatorTop />
+        <SectionSeparator position="top" />
 
         <Media />
 
-        <SectionSeparatorBottom />
+        <SectionSeparator position="bottom" />
       </section>
 
       <TextMarquee type="ignite" />
