0c87cbf396105076c2edb1f0044b277c15079dbe
Mick Suraj <127860227+Micksuraj777@users.noreply.github.com>
2025-12-07 19:26:08 +0530

    resolved version issues

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
 import Committee from './components/comitee';
 import TextMarquee from '@/components/marquee';
 import Image from 'next/image';
