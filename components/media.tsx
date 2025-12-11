66b50599c0b32bc16eb283ca9a5a69e8a1429935
DjRxxstr <dhyanj2016@gmail.com>
2025-12-11 14:32:41 +0530

    rem changed to tailwind classes, other minor changes

--- components/media.tsx
+++ components/media.tsx
@@ -1,7 +1,7 @@
 export default function Media() {
   return (
     <div className="relative mx-auto flex h-[60vh] max-w-screen-2xl flex-col items-center justify-center gap-3 overflow-hidden">
-      <div className="flex items-center justify-center text-center font-orbitron text-[3rem] font-bold text-secondary xs:text-[4rem]">
+      <div className="flex items-center justify-center text-center font-orbitron text-5xl font-bold text-secondary xs:text-6xl">
         <div className="w-full break-words leading-none">MEDIA PARTNER</div>
       </div>
 
